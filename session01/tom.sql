21b09bef578409f16cffa6a8017772be3a1988ce
RJM <r.j.m.degroot@student.utwente.nl>
2014-10-12 15:58:43 +0200

    1D

--- session01/tom.sql
+++ session01/tom.sql
@@ -8,3 +8,14 @@ thema_id INT,
 docent_id int,
 naam VARCHAR
 );
+
+INSERT INTO "Docent" VALUES(1,'Dr.Maurice');
+INSERT INTO "Docent" VALUES(2,'Dr.Klaas');
+INSERT INTO "Docent" VALUES(3,'Dr.Luis');
+INSERT INTO "Docent" VALUES(4,'Dr.Djoerd');
+INSERT INTO "Thema" VALUES(1,1,'Xml');
+INSERT INTO "Thema" VALUES(2,2,'uml');
+INSERT INTO "Thema" VALUES(3,3,'jsp');
+INSERT INTO "Thema" VALUES(4,3,'rest');
+INSERT INTO "Thema" VALUES(5,4,'sql');
+INSERT INTO "Thema" VALUES(6,4,'git');
