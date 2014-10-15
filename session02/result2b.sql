12b0a8513391018a0330c0171e0c74a734db7a27
RJM <r.j.m.degroot@student.utwente.nl>
2014-10-15 16:17:10 +0200

    shunting rule

--- session02/result2b.sql
+++ session02/result2b.sql
@@ -1,8 +1,6 @@
 SELECT p.pid
-FROM Person p
-WHERE EXISTS(
-SELECT * FROM Acts a, Movie M
+FROM Person p, Acts a, Movie M
 WHERE a.pid = p.pid
 AND M.mid=a.mid 
 AND'Back to the Future'=M.name
-);
+;
