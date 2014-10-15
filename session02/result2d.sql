883916f84f2848c19cc404ccd38e0aedb28fec5e
RJM <r.j.m.degroot@student.utwente.nl>
2014-10-15 17:03:46 +0200

    2d, stap 2

--- session02/result2d.sql
+++ session02/result2d.sql
@@ -1,3 +1,4 @@
 Select P.name
-From Person P
-Where hij heeft alleen maar regiseurloze films geschreven.
+From Person P, Writer W, Movies M, Directs D
+Where P.pid is W.pid and
+er bestaat geen film die w.mid =d.mid heeft.
