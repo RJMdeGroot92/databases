e76f858b5aa312712d2fb39f84881aefba0d6353
RJM <r.j.m.degroot@student.utwente.nl>
2014-10-15 16:54:22 +0200

    2c, foreign key rule

--- session02/result2c.sql
+++ session02/result2c.sql
@@ -1,9 +1,7 @@
 select P.name
-from Person P, writes W, Movie M
-where P.pid=W.pid
-and W.mid=M.mid
-and not exists(
+from Person P, writes W
+where P.pid=W.pid and not exists(
 select D.mid
 from Directs D
-where D.mid=M.mid)
+where D.mid=W.mid)
 ;
