627b21afa222f6f813fb3571e385cb2381652ea8
RJM <r.j.m.degroot@student.utwente.nl>
2014-10-15 15:02:07 +0200

    One point rule:

--- session02/result2b.sql
+++ session02/result2b.sql
@@ -3,9 +3,8 @@ FROM Person p
 WHERE EXISTS(
 SELECT * FROM Acts a
 WHERE a.pid = p.pid
-AND EXISTS (
-SELECT * FROM Movie m -- the movies exists and a plays in it
+AND 'Back to the Future' in (
+SELECT m.name FROM Movie m -- the movies exists and a plays in it
 WHERE m.mid = a.mid
-AND m.name = 'Back to the Future'
 )
 );
